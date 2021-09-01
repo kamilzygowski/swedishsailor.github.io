84c72376806656b05e17246e1be146d44145fcfa
swedishsailor <kzygowski7@gmail.com>
2021-09-02 01:47:24 +0200

    browser changed to hash

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Home from '../Home/Home';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { HashRouter as Router, Switch, Route} from 'react-router-dom';
 import SalveDevito from '../GamesSites/SalveDevito.js';
 import DimensionEye from '../GamesSites/DimensionEye';
 import SdHeroes from '../GamesSites/SdHeroes';
