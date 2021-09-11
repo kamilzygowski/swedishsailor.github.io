fb4cf8d55522bc0c15c74817b2fa57085a51aca7
swedishsailor <kzygowski7@gmail.com>
2021-09-11 13:50:12 +0200

    milionlasers works

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Home from '../Home/Home';
-import { HashRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import SalveDevito from '../GamesSites/SalveDevito.js';
 import DimensionEye from '../GamesSites/DimensionEye';
 import SdHeroes from '../GamesSites/SdHeroes';
@@ -14,7 +14,7 @@ class App extends React.Component {
                 <Switch>
                     <Route path="/" component={Home} exact />
                     <Route path="/salvedevito" component={SalveDevito} exact/>
-                    <Route path="/#/milionlasers" component={MilionLasers} exact/>
+                    <Route path="/milionlasers" component={MilionLasers} exact/>
                     <Route path="/sd-heroes" component={SdHeroes} exact/>
                     <Route path="/#/dimensioneye" component={DimensionEye} exact/>
                     
