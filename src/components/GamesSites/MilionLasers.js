a31cb937dc8dfacc6c7fefdf5e5e2a5f2dce4c01
swedishsailor <kzygowski7@gmail.com>
2021-09-11 13:20:57 +0200

    still trying

--- src/components/GamesSites/MilionLasers.js
+++ src/components/GamesSites/MilionLasers.js
@@ -2,13 +2,15 @@ import React from "react";
 import Unity, { UnityContent } from 'react-unity-webgl';
 
 const unityContent = new UnityContent({
-    loaderUrl: 'MilionLasers/Build/UnityLoader.js',
-    dataUrl: 'MilionLasers/Build/MilionLasers.data.unityweb',
-    frameworkUrl: 'MilionLasers/Build/MilionLasers.wasm.framework.unityweb',
-    codeUrl: 'MilionLasers/Build/MilionLasers.wasm.code.unityweb',
+    loaderUrl: './MilionLasers/Build/UnityLoader.js',
+    dataUrl: './MilionLasers/Build/MilionLasers.data.unityweb',
+    frameworkUrl: './MilionLasers/Build/MilionLasers.wasm.framework.unityweb',
+    codeUrl: './MilionLasers/Build/MilionLasers.wasm.code.unityweb',
   });
+  
 
 const MilionLasers = () => (
+    
     <div id='/milionlasers'>
         <Unity unityContent={unityContent}
         />
