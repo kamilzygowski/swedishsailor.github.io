eaa398caf60c0f88dea02783957b3627b96c9c24
swedishsailor <kzygowski7@gmail.com>
2021-09-12 12:16:44 +0200

    SalveDevito section in progress

--- src/components/GamesSites/SalveDevito.js
+++ src/components/GamesSites/SalveDevito.js
@@ -1,10 +1,22 @@
 import React from "react";
 
 const SalveDevito = () => (
-    <div id='/salvedevito'>
+    <div className="gamesIntro" id='/salvedevito'>
                 <h2 className="gamesTitle"> SalveDevito </h2>
-        <p className="gamesAbout"> Salve Devito is an isometric online rpg game</p>
+        <p className="gamesAbout"> Salve Devito is an isometric online rpg game, where you can choose what to do from huge variety of activities.</p>
         <h3 className="gamesOnMobile"> To play the game and to see all the stuff you have to display this site on your Computer :( </h3>
+        <br/>
+        <br/>
+        <div className="imgTextWrapper">
+        <img className="gamesImg1" src="https://i.postimg.cc/N011YwK1/sddp.png"></img>
+        <p className="imgDesc1"> Here on the left image you can see the main temple of Salve Devito in city Lapide, where every player is respawning. <br/> Also you can notice dead body 
+        at the entrance, guess what happened  {'>'}:) </p>
+        </div>
+        <div className="imgTextWrapper">
+        <p className="imgDesc2"> On the right image is how our game looks like in unity engine. <br/> The engine is a place where the "magic" happens and where our creativity
+        is leaking out! </p>
+        <img className="gamesImg2" src="https://i.postimg.cc/vB0xH8b6/salvedevito2.png"></img>
+        </div>
     </div>
 );
 
